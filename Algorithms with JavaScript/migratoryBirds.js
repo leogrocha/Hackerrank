0c04265450205700f17ee9cdd940b00efbaf5869
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-08-11 14:57:16 -0300

    Update migratoryBirds.js

--- Algorithms with JavaScript/migratoryBirds.js
+++ Algorithms with JavaScript/migratoryBirds.js
@@ -1,16 +1,30 @@
 function migratoryBirds(arr) {
 
     let array = [];
+    let sum = 1;
     
     // ordenando o arr
     arr.sort((a, b) => a - b);
 
     //[ 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 5 ]
     for(let i = 0; i < arr.length; i++){
-
+        if(arr[i] === arr[i+1]){
+            sum++;
+        } else {
+            array.push({
+                id: arr[i],
+                qtde_elementos: sum,
+            })
+            sum = 1;
+        }
     }
 
-    return arr;
+    for(let j = 0; j < array.length; j++){
+        console.log(array[j]);
+    }
+
+
+    return array;
 }
 
 console.log(migratoryBirds([1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]));
