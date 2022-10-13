e1deb1b1d8a61848b9c9d2749869206559c26401
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-10-13 00:25:49 -0300

    Finish utopianTree

--- Algorithms with JavaScript/utopianTree.js
+++ Algorithms with JavaScript/utopianTree.js
@@ -1,32 +1,19 @@
 function utopiaTree(n) {
 
-    let arrayPeriodos = [];
+    
     let value = 0;
-    let bigger = 0;
-    // verificar qual o maior valoe dos períodos
-    for (let i = 0; i < n.length; i++) {
-        if (n[i] > bigger) {
-            bigger = n[i];
-        }
-    }
 
-    for (let i = 0; i < bigger; i++) {
+    for (let i = 0; i <= n; i++) {
         if (i % 2 == 0) {
             value += 1;
-            arrayPeriodos.push(value);
         } else {
             value *= 2;
-            arrayPeriodos.push(value);
         }
     }
 
-    console.log(arrayPeriodos);
-
-    
-
-    for(let i = 0; i < n.length; i++) {
-        console.log(arrayPeriodos[n[i]]);
-    }
+    return value;
 }
 
-utopiaTree([2, 0, 1]);
+console.log(utopiaTree(4));
+
+
