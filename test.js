7122c6038d1870919fe19190da1e5fbf2c6b7fbb
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-11-13 16:05:05 -0300

    Finalizando o exercício cutTheSticks e adicionando arquivo de teste

--- test.js
+++ test.js
@@ -1,27 +1,31 @@
-function equalizeArray(arr) {
-    var count = 0;
-    var nums = [];
+// function equalizeArray(arr) {
+//     var count = 0;
+//     var nums = [];
     
-    arr.sort((a, b) => a - b);
+//     arr.sort((a, b) => a - b);
     
-    for (let index = 0; index < arr.length; index++) {
-        if(arr[index+1] != undefined && arr[index] == arr[index+1]) {
-            count++;
-        } else {
-            count++;
-            nums.push({
-                value: arr[index],
-                quantity: count
-            })
+//     for (let index = 0; index < arr.length; index++) {
+//         if(arr[index+1] != undefined && arr[index] == arr[index+1]) {
+//             count++;
+//         } else {
+//             count++;
+//             nums.push({
+//                 value: arr[index],
+//                 quantity: count
+//             })
 
-            count = 0;
-        }
+//             count = 0;
+//         }
         
-    }
+//     }
 
-    return nums;
-}
+//     return nums;
+// }
 
-console.log(equalizeArray([3, 3, 2, 1, 3]));
+// console.log(equalizeArray([3, 3, 2, 1, 3]));
 
+const arr = [5, 4, 4, 2, 2, 8];
+arr.sort((a, b) => a - b);
+arr.splice(0, 1);
 
+console.log(arr);
