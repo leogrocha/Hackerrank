09ea47a0c56ecc29f27de2f03132c629dcd4fc75
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-11-14 14:13:20 -0300

    Finalizando miniumDistances

--- test.js
+++ test.js
@@ -1,29 +1,3 @@
-// function equalizeArray(arr) {
-//     var count = 0;
-//     var nums = [];
-    
-//     arr.sort((a, b) => a - b);
-    
-//     for (let index = 0; index < arr.length; index++) {
-//         if(arr[index+1] != undefined && arr[index] == arr[index+1]) {
-//             count++;
-//         } else {
-//             count++;
-//             nums.push({
-//                 value: arr[index],
-//                 quantity: count
-//             })
+const nums = [10,1,3,0];
 
-//             count = 0;
-//         }
-        
-//     }
-
-//     return nums;
-// }
-
-// console.log(equalizeArray([3, 3, 2, 1, 3]));
-
-for (let index = 2; index <= 9; index++) {
-    console.log(` ${index} % 8 = ${index % 8}`);
-}
+console.log(Math.min(...nums));
