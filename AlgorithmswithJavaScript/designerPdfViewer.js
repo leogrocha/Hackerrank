d1c97cbf972559f172853bfaa567d34e4771de6a
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-11-13 23:39:26 -0300

    Finalizando DesignerPDFViewer

--- AlgorithmswithJavaScript/designerPdfViewer.js
+++ AlgorithmswithJavaScript/designerPdfViewer.js
@@ -1,22 +1,15 @@
-function designerPdfViewer(w, word) {
-    let alphabetic = [];
-    let arrayWord = [];
-    for (let i = 65; i <= 90; i++) {
-        var letra = String.fromCharCode(i);
-        alphabetic.push(letra.toLowerCase());
+function designerPdfViewer(h, word) {
+    const quantidadeCartas = word.length;
+    var alfabeto = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var maiorValor = 0;
+
+    for(let index = 0; index < word.length; index++) {
+        let posicaoAlfabeto = alfabeto.findIndex((item) => item == word[index]);
+        let item = h[posicaoAlfabeto];
+        maiorValor = item > maiorValor ? item : maiorValor;
     }
 
-    for (let i = 0; i < word.length; i++) {
-        arrayWord.push(word[i]);
-    }
-
-    for (let index in alphabetic) {
-        let item = alphabetic[index];
-        
-    }
-
-    return arrayWord;
+    return quantidadeCartas * maiorValor;
 }
 
-console.log(designerPdfViewer([1, 3 ,1 ,3 ,1 ,4 ,1 ,3 ,2 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ], 'abc'));
-
+console.log(designerPdfViewer([1,3,1 ,3 ,1 ,4 ,1 ,3 ,2 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,7], 'zaba'));
