e2875ed5e69ef1d5037349b3e06916c0285e896f
leogrocha <leogrocha1998@gmail.com>
2023-11-22 17:50:33 -0300

    Finalizando MarsExploration

--- AlgorithmswithJavaScript/marsExploration.js
+++ AlgorithmswithJavaScript/marsExploration.js
@@ -1,13 +1,22 @@
 function marsExploration(s) {
-    const sos = "SOS";
-    const palavraFormatada = s.replaceAll(sos, "");
+  const sos = "SOS";
+  let count = 0;
+  let strings = [];
 
-    console.log(palavraFormatada);
+  for (let i = 0; i < s.length; i += 3) {
+    strings.push(s.slice(i, i + 3));
+  }
 
-    return palavraFormatada.length / sos.length;
+  for (let i = 0; i < strings.length; i++) {
+    count = strings[i][0] != sos[0] ? count + 1 : count;
+    count = strings[i][1] != sos[1] ? count + 1 : count;
+    count = strings[i][2] != sos[2] ? count + 1 : count;
+  }
+
+  return count;
 }
 
 // console.log(marsExploration('SOSSPSSQSSOR'));
 // console.log(marsExploration('SOSSOSSOS'));
-console.log(marsExploration('SOSOOSOSOSOSOSSOSOSOSOSOSOS'));
-
+console.log(marsExploration("SOSOOSOSOSOSOSSOSOSOSOSOSOS"));
+// console.log(marsExploration('SOSSOT'));
--- test.js
+++ test.js
@@ -1,8 +1,19 @@
-pages = [
-    [1,2,3]
-]
+const sos = "SOS";
+let message = "SOSOOSOSOSOSOSSOSOSOSOSOSOS";
+var strings = [];
 
-// pages[i].some((value) => value === i+1);
+for(let i = 0; i < message.length; i+=3) {
+    strings.push(message.slice(i, i+3));
+}
 
-let existeValorIgual = pages[0].some((value) => value === 1);
-console.log(existeValorIgual);
+console.log(strings);
+
+let count = 0;
+
+for(let i = 0; i < strings.length; i++) {
+    count = strings[i][0] != sos[0] ? count + 1 : count;
+    count = strings[i][1] != sos[1] ? count + 1 : count;
+    count = strings[i][2] != sos[2] ? count + 1 : count;
+}
+
+console.log("COUNT: ", count);
